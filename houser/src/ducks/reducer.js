8df79ff60f5ceb5466d865d8e2019da148c339cd
Wanda Nataly <uzcateguiw@hotmail.com>
2018-05-11 15:36:32 -0600

    Done

--- houser/src/ducks/reducer.js
+++ houser/src/ducks/reducer.js
@@ -1,4 +1,4 @@
-const initialState={
+const initialState = {
     name: '',
     addres: '',
     city: '',
@@ -12,52 +12,56 @@ const UPDATE_CITY = "UPDATE_CITY";
 const UPDATE_STATE = "UPDATE_STATE";
 const UPDATE_ZIPCODE = "UPDATE_ZIPCODE";
 
-function reducer(state= initialState, action){
-    switch(action.type){
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
         case UPDATE_NAME:
-        return Object.assign({}, state, {name: action.payload});
+            return Object.assign({}, state, { name: action.payload});
+
         case UPDATE_ADDRESS:
-        return Object.assign({}, state, {address: action.payload});
+            return Object.assign({}, state, { address: action.payload});
+
         case UPDATE_CITY:
-        return Object.assign({}, state, {city: action.payload});
+            return Object.assign({}, state, { city: action.payload});
+
         case UPDATE_STATE:
-        return Object.assign({}, state, {state: action.payload});
+            return Object.assign({}, state, { state: action.payload});
+
         case UPDATE_ZIPCODE:
-        return Object.assign({}, state, {zipcode: action.payload});
-        Default:
-        return state;
+            return Object.assign({}, state, { zipcode: action.payload});
+
+            Default:
+            return state;
     }
 }
 
-export function updateName(){
-    return{
+export function updateName() {
+    return {
         type: UPDATE_NAME,
-        payload: name,
+        payload: name
     }
 }
-export function updateAddress(){
-    return{
+export function updateAddress() {
+    return {
         type: UPDATE_ADDRESS,
-        payload: address,
+        payload: address
     }
 }
-export function updateCity(){
-    return{
+export function updateCity() {
+    return {
         type: UPDATE_CITY,
-        payload: city,
+        payload: city
     }
 }
-export function updateState(){
-    return{
+export function updateState() {
+    return {
         type: UPDATE_STATE,
         payload: state
     }
 }
-export function updateZipcode(){
-    return{
+export function updateZipcode() {
+    return {
         type: UPDATE_ZIPCODE,
-        payload: zipcode,
+        payload: zipcode
     }
 }
 
-export default Reducer;
--- houser/src/routes.js
+++ houser/src/routes.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Dashboard from './component/Dashboard/Dashboard';
 import Wizard from './component/Wizard/Wizard';
